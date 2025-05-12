bfa4a907c5302544d35bf6243339e8b582184b54
bellamyphan <bellamyphan@icloud.com>
2025-05-12 17:22:38 -0500

    feat: implement MainMenuComponent with routing and initial template

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from './component/main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
+  { path: 'main-menu', component: MainMenuComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
