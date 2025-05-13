12dad8a53fb4fbb1d67f7dad46a2c6a040e1768c
BellamyPhan <bellamyphan@icloud.com>
2025-05-12 23:45:48 -0500

    backend: add model classes Transaction and update Bank

--- spring-backend/src/main/java/com/bellamyphan/spring_backend/model/Bank.java
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/model/Bank.java
@@ -16,12 +16,17 @@ public class Bank {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String name;
+
     @Column(name = "opening_date", nullable = false)
     private LocalDate openingDate;
+
     @Column(name = "closing_date")
     private LocalDate closingDate;
+
+    // Bank must have a bank type
     @ManyToOne(optional = false, fetch = FetchType.LAZY) // Foreign key relationship
     @JoinColumn(name = "type_id", referencedColumnName = "id") // Foreign key column in 'transaction_banks' table
     private BankType type;
---
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/model/Transaction.java
@@ -0,0 +1,49 @@
+package com.bellamyphan.spring_backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data // Lombok will generate getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    @NotNull
+    private LocalDate date;
+
+    @Column(name = "amount", nullable = false)
+    @NotNull
+    private Double amount;
+
+    // Some transactions do not have a type, only track the bank for bank balances
+    @ManyToOne(optional = false, fetch = FetchType.LAZY) // Foreign key relationship
+    @JoinColumn(name = "type_id", referencedColumnName = "id") // Foreign key column in 'transactions' table
+    private TransactionType type;
+
+    @Column(name = "notes")
+    private String notes;
+
+    // Some transactions do not need a bank, simply to track transaction_type
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_id", referencedColumnName = "id") // Foreign key column in 'transactions' table
+    private Bank bank;
+
+    // Constructor with parameters excluding id
+    public Transaction(LocalDate date, Double amount, TransactionType type, String notes, Bank bank) {
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+        this.notes = notes;
+        this.bank = bank;
+    }
+}
