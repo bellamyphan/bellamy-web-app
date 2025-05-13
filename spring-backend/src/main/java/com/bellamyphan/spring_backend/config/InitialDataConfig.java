f5ebd9db6af21f49e7ba52d9fc53d07691911975
BellamyPhan <bellamyphan@icloud.com>
2025-05-13 01:11:03 -0500

    backend: create InitialDataConfig.java

---
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/config/InitialDataConfig.java
@@ -0,0 +1,30 @@
+package com.bellamyphan.spring_backend.config;
+
+import com.bellamyphan.spring_backend.service.RoleService;
+import com.bellamyphan.spring_backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class InitialDataConfig {
+
+    private final RoleService roleService;
+    private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(InitialDataConfig.class);
+
+    @Bean
+    public CommandLineRunner initData() {
+        return (args) -> {
+            logger.info("Creating roles...");
+            roleService.createFirstRole();
+
+            logger.info("Creating first user...");
+            userService.createFirstUser();
+        };
+    }
+}
