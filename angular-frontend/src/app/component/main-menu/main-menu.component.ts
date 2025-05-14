0b84f316826fbb64c72a89fa0160b5587420cfe2
BellamyPhan <bellamyphan@icloud.com>
2025-05-14 08:41:52 -0500

    frontend: setup url and button to download resume

--- angular-frontend/src/app/component/main-menu/main-menu.component.ts
+++ angular-frontend/src/app/component/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppConfig } from '../../app-config';
 
 @Component({
   selector: 'app-main-menu',
@@ -23,13 +24,22 @@ export class MainMenuComponent {
     window.location.href = '/tic-tac-toe';
   }
 
+  downloadResume() {
+    const baseUrl = AppConfig.springApiUrl;
+    const link = document.createElement('a');
+    link.href = baseUrl + "/api/resume";
+    link.download = 'resume.pdf'; // optional, helps hint filename
+    link.target = '_blank';
+    link.click();
+  }
+
   sendEmail(): void {
     const subject = 'Hello Bellamy from the Bellamy Web App';
     const body = 'I hope you are doing well. This is a test email from the Bellamy Web App.';
     const mailtoLink = `mailto:bellamyphan@icloud.com
       ?subject=${encodeURIComponent(subject)}
       &body=${encodeURIComponent(body)}`;
-      window.location.href = mailtoLink;
+    window.location.href = mailtoLink;
   }
 
 }
