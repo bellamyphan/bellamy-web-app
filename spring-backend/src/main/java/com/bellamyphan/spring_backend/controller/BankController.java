f6c9f7700d1ea87214fd11a5191af6c47c36c359
BellamyPhan <bellamyphan@icloud.com>
2025-05-14 10:04:48 -0500

    backend: BankController remove the bank id so that Jpa will handle id automatically

--- spring-backend/src/main/java/com/bellamyphan/spring_backend/controller/BankController.java
+++ spring-backend/src/main/java/com/bellamyphan/spring_backend/controller/BankController.java
@@ -1,10 +1,16 @@
 package com.bellamyphan.spring_backend.controller;
 
 import com.bellamyphan.spring_backend.model.Bank;
+import com.bellamyphan.spring_backend.model.User;
 import com.bellamyphan.spring_backend.repository.BankRepository;
+import com.bellamyphan.spring_backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BankController {
 
+    private static final Logger logger = LoggerFactory.getLogger(BankController.class);
+
     private final BankRepository bankRepository;
+    private final UserRepository userRepository;
 
     @PostMapping
     ResponseEntity<Bank> createNewBank(@RequestBody Bank bank) {
+        // Get user by the jwt token
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Cannot find the user with username: " + username));
+        bank.setUser(user);
+        bank.setId(null);
         Bank newBank = bankRepository.save(bank);
+        logger.info("Username {} created a new bank: {}", username, newBank);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBank);
     }
 }
